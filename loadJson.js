4fb1396b7bfe7c684aa6a60aeb73baf82d3ffb9b
fabian sguiglia <fsguiglia@gmail.com>
2024-08-05 17:23:45 -0300

    Load from embedded dict

--- loadJson.js
+++ loadJson.js
@@ -1,6 +1,22 @@
 inlets = 1;
 outlets = 2;
 var memstr;
+var _recall = new Dict(jsarguments[1]);
+
+function loadFromDict()
+{
+	keys = _recall.getkeys();
+	if(keys != null)
+	{
+		for(i = 0; i < keys.length; i++)
+		{
+			var a = _recall.get(keys[i]);
+			outlet(0, a);
+			outlet(1, i);
+		}
+	}
+	
+}
 
 function load(path)
 {
@@ -23,6 +39,7 @@ function load(path)
 	
 	for(i = 0; i < data["files"].length; i++)
 	{
+		_recall.set(String(i), data["files"][i]);
 		outlet(0, data["files"][i]);
 		outlet(1, i);
 	}
